4036fa142e99a2e28b70ed529d6c8f97198a3c6e
lewis he <lewisxhe@outlook.com>
2020-07-31 13:55:36 +0800

    Update sensor to mpu9250

--- examples/FactoryTest/FactoryTest.ino
+++ examples/FactoryTest/FactoryTest.ino
@@ -1,13 +1,16 @@
-#include <SPI.h>
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
 #include <pcf8563.h>
 #include <SoftSPI.h>
 #include <SerialFlash.h>
-#include "charge.h"
 #include <Wire.h>
-// Include the SparkFunLSM9DS1 library and its dependencies.
-#include <SparkFunLSM9DS1.h>
+#include <SparkFunMPU9250-DMP.h>
+#include <nrf_nvic.h>
+#include <nrf_sdm.h>
+#include <nrf_soc.h>
+#include "nrf_rtc.h"
+
+#include "charge.h"
 
 #define SPI_FLASH_MOSI  16
 #define SPI_FLASH_MISO  11
@@ -36,7 +39,7 @@
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 PCF8563_Class rtc;
-LSM9DS1 imu; // Create an LSM9DS1 object to use from here on.
+MPU9250_DMP imu;
 
 uint8_t omm = 99;
 uint32_t targetTime = 0;       // for next 1 second timeout
@@ -46,12 +49,17 @@ uint8_t func_select = 0;
 bool pressed = false;
 bool charge_indication = false;
 bool found_imu = false, found_rtc = false, rtcIrq = false;
+bool sleep_enable = false;
+volatile bool event = false;
 
 uint32_t vbat_pin = PIN_VBAT;
 #define VBAT_MV_PER_LSB   (0.73242188F)   // 3.0V ADC range and 12-bit ADC resolution = 3000mV/4096
 #define VBAT_DIVIDER      (0.71275837F)   // 2M + 0.806M voltage divider on VBAT = (2M / (0.806M + 2M))
 #define VBAT_DIVIDER_COMP (1.403F)        // Compensation factor for the VBAT divider
 #define REAL_VBAT_MV_PER_LSB (VBAT_DIVIDER_COMP * VBAT_MV_PER_LSB)
+
+
+void setSleepEnabled(bool enabled);
 
 float readVBAT(void)
 {
@@ -73,127 +81,52 @@ float readVBAT(void)
     return raw * REAL_VBAT_MV_PER_LSB;
 }
 
-// configureIMU sets up our LSM9DS1 interface, sensor scales
-// and sample rates.
-uint16_t configureIMU()
-{
-    // gyro.latchInterrupt controls the latching of the
-    // gyro and accelerometer interrupts (INT1 and INT2).
-    // false = no latching
-    imu.settings.gyro.latchInterrupt = false;
-
-    // Set gyroscope scale to +/-245 dps:
-    imu.settings.gyro.scale = 245;
-    // Set gyroscope (and accel) sample rate to 14.9 Hz
-    imu.settings.gyro.sampleRate = 1;
-    // Set accelerometer scale to +/-2g
-    imu.settings.accel.scale = 2;
-    // Set magnetometer scale to +/- 4g
-    imu.settings.mag.scale = 4;
-    // Set magnetometer sample rate to 0.625 Hz
-    imu.settings.mag.sampleRate = 0;
-
-    // Call imu.begin() to initialize the sensor and instill
-    // it with our new settings.
-    return imu.begin(LSM9DS1_AG_ADDR(1), LSM9DS1_M_ADDR(1), Wire); // set addresses and wire port
-}
-
-void configureLSM9DS1Interrupts()
-{
-    /////////////////////////////////////////////
-    // Configure INT1 - Gyro & Accel Threshold //
-    /////////////////////////////////////////////
-    // For more information on setting gyro interrupt, threshold,
-    // and configuring the intterup, see the datasheet.
-    // We'll configure INT_GEN_CFG_G, INT_GEN_THS_??_G,
-    // INT_GEN_DUR_G, and INT1_CTRL.
-    // 1. Configure the gyro interrupt generator:
-    //  - ZHIE_G: Z-axis high event (more can be or'd together)
-    //  - false: and/or (false = OR) (not applicable)
-    //  - false: latch interrupt (false = not latched)
-    imu.configGyroInt(ZHIE_G, false, false);
-    // 2. Configure the gyro threshold
-    //   - 500: Threshold (raw value from gyro)
-    //   - Z_AXIS: Z-axis threshold
-    //   - 10: duration (based on ODR)
-    //   - true: wait (wait duration before interrupt goes low)
-    imu.configGyroThs(500, Z_AXIS, 10, true);
-    // 3. Configure accelerometer interrupt generator:
-    //   - XHIE_XL: x-axis high event
-    //     More axis events can be or'd together
-    //   - false: OR interrupts (N/A, since we only have 1)
-    imu.configAccelInt(XHIE_XL, false);
-    // 4. Configure accelerometer threshold:
-    //   - 20: Threshold (raw value from accel)
-    //     Multiply this value by 128 to get threshold value.
-    //     (20 = 2600 raw accel value)
-    //   - X_AXIS: Write to X-axis threshold
-    //   - 10: duration (based on ODR)
-    //   - false: wait (wait [duration] before interrupt goes low)
-    imu.configAccelThs(20, X_AXIS, 1, false);
-    // 5. Configure INT1 - assign it to gyro interrupt
-    //   - XG_INT1: Says we're configuring INT1
-    //   - INT1_IG_G | INT1_IG_XL: Sets interrupt source to
-    //     both gyro interrupt and accel
-    //   - INT_ACTIVE_LOW: Sets interrupt to active low.
-    //         (Can otherwise be set to INT_ACTIVE_HIGH.)
-    //   - INT_PUSH_PULL: Sets interrupt to a push-pull.
-    //         (Can otherwise be set to INT_OPEN_DRAIN.)
-    imu.configInt(XG_INT1, INT1_IG_G | INT_IG_XL, INT_ACTIVE_LOW, INT_PUSH_PULL);
-
-    ////////////////////////////////////////////////
-    // Configure INT2 - Gyro and Accel Data Ready //
-    ////////////////////////////////////////////////
-    // Configure interrupt 2 to fire whenever new accelerometer
-    // or gyroscope data is available.
-    // Note XG_INT2 means configuring interrupt 2.
-    // INT_DRDY_XL is OR'd with INT_DRDY_G
-    imu.configInt(XG_INT2, INT_DRDY_XL | INT_DRDY_G, INT_ACTIVE_LOW, INT_PUSH_PULL);
-
-    //////////////////////////////////////
-    // Configure Magnetometer Interrupt //
-    //////////////////////////////////////
-    // 1. Configure magnetometer interrupt:
-    //   - XIEN: axis to be monitored. Can be an or'd combination
-    //     of XIEN, YIEN, or ZIEN.
-    //   - INT_ACTIVE_LOW: Interrupt goes low when active.
-    //   - true: Latch interrupt
-    imu.configMagInt(XIEN, INT_ACTIVE_LOW, true);
-    // 2. Configure magnetometer threshold.
-    //   There's only one threshold value for all 3 mag axes.
-    //   This is the raw mag value that must be exceeded to
-    //   generate an interrupt.
-    imu.configMagThs(10000);
-
-}
-
 bool setupIMU()
 {
-    // Set up our Arduino pins connected to interrupts.
-    // We configured all of these interrupts in the LSM9DS1
-    // to be active-low.
-    pinMode(INT2_PIN_DRDY, INPUT_PULLUP);
-    pinMode(INT1_PIN_THS, INPUT_PULLUP);
-    pinMode(INTM_PIN_THS, INPUT_PULLUP);
-    // The magnetometer DRDY pin (RDY) is not configurable.
-    // It is active high and always turned on.
-    pinMode(RDYM_PIN, INPUT);
-
     Wire.begin();
 
-    // Turn on the IMU with configureIMU() (defined above)
-    // check the return status of imu.begin() to make sure
-    // it's connected.
-    uint16_t status = configureIMU();
-    if (status == false) {
-        Serial.print("Failed to connect to IMU: 0x");
-        Serial.println(status, HEX);
+    // Call imu.begin() to verify communication with and
+    // initialize the MPU-9250 to it's default values.
+    // Most functions return an error code - INV_SUCCESS (0)
+    // indicates the IMU was present and successfully set up
+    if (imu.begin() != INV_SUCCESS) {
+        Serial.println("Unable to communicate with MPU-9250");
+        Serial.println("Check connections, and try again.");
+        Serial.println();
         return false;
     }
 
-    // After turning the IMU on, configure the interrupts:
-    configureLSM9DS1Interrupts();
 
+    // Use setSensors to turn on or off MPU-9250 sensors.
+    // Any of the following defines can be combined:
+    // INV_XYZ_GYRO, INV_XYZ_ACCEL, INV_XYZ_COMPASS,
+    // INV_X_GYRO, INV_Y_GYRO, or INV_Z_GYRO
+    // Enable all sensors:
+    imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
+
+    // Use setGyroFSR() and setAccelFSR() to configure the
+    // gyroscope and accelerometer full scale ranges.
+    // Gyro options are +/- 250, 500, 1000, or 2000 dps
+    imu.setGyroFSR(2000); // Set gyro to 2000 dps
+    // Accel options are +/- 2, 4, 8, or 16 g
+    imu.setAccelFSR(2); // Set accel to +/-2g
+    // Note: the MPU-9250's magnetometer FSR is set at
+    // +/- 4912 uT (micro-tesla's)
+
+    // setLPF() can be used to set the digital low-pass filter
+    // of the accelerometer and gyroscope.
+    // Can be any of the following: 188, 98, 42, 20, 10, 5
+    // (values are in Hz).
+    imu.setLPF(5); // Set LPF corner frequency to 5Hz
+
+    // The sample rate of the accel/gyro can be set using
+    // setSampleRate. Acceptable values range from 4Hz to 1kHz
+    imu.setSampleRate(10); // Set sample rate to 10Hz
+
+    // Likewise, the compass (magnetometer) sample rate can be
+    // set using the setCompassSampleRate() function.
+    // This value can range between: 1-100Hz
+    imu.setCompassSampleRate(10); // Set mag rate to 10Hz
     return true;
 }
 
@@ -207,11 +140,10 @@ bool setupDisplay()
     return true;
 }
 
+
 void setup()
 {
     Serial.begin(115200);
-
-    delay(5000);
 
     Serial.println("Start...");
 
@@ -220,6 +152,7 @@ void setup()
     pinMode(TP_PIN_PIN, INPUT);
 
     digitalWrite(TFT_BL, HIGH);
+
     digitalWrite(TOUCH_PW, HIGH);
 
     setupDisplay();
@@ -288,6 +221,8 @@ void setup()
     }
 
     tft.fillScreen(ST77XX_BLACK);
+
+
 }
 
 
@@ -296,7 +231,6 @@ void RTC_Show()
     static int16_t x, y;
     if (targetTime < millis()) {
         RTC_Date datetime = rtc.getDateTime();
-        Serial.println(millis());
         hh = datetime.hour;
         mm = datetime.minute;
         ss = datetime.second;
@@ -356,38 +290,39 @@ void RTC_Show()
 
 void IMU_Show()
 {
-    // Update the sensor values whenever new data is available
-    if ( imu.gyroAvailable() ) {
-        // To read from the gyroscope,  first call the
-        // readGyro() function. When it exits, it'll update the
-        // gx, gy, and gz variables with the most current data.
-        imu.readGyro();
+    // dataReady() checks to see if new accel/gyro data
+    // is available. It will return a boolean true or false
+    // (New magnetometer data cannot be checked, as the library
+    //  runs that sensor in single-conversion mode.)
+    if ( imu.dataReady() ) {
+        // Call update() to update the imu objects sensor data.
+        // You can specify which sensors to update by combining
+        // UPDATE_ACCEL, UPDATE_GYRO, UPDATE_COMPASS, and/or
+        // UPDATE_TEMPERATURE.
+        // (The update function defaults to accel, gyro, compass,
+        //  so you don't have to specify these values.)
+        imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
+
+        tft.setTextColor(ST77XX_GREEN, ST77XX_BLACK);
+        tft.fillScreen(ST77XX_BLACK);
+        tft.setTextSize(1);
+        tft.setCursor(0, 0);
+        tft.print("ACC x:"); tft.print( imu.calcAccel(imu.ax)); tft.print(" y:"); tft.print(imu.calcAccel(imu.ay)); tft.print(" z:"); tft.println(imu.calcAccel(imu.az));
+        tft.print("GYR x:"); tft.print( imu.calcGyro(imu.gx)); tft.print(" y:"); tft.print(imu.calcGyro(imu.gy)); tft.print(" z:"); tft.println(imu.calcGyro(imu.gz));
+        tft.print("MAG x:"); tft.print( imu.calcMag(imu.mx)); tft.print(" y:"); tft.print(imu.calcMag(imu.my)); tft.print(" z:"); tft.println(imu.calcMag(imu.mz));
+
+
     }
-    if ( imu.accelAvailable() ) {
-        // To read from the accelerometer, first call the
-        // readAccel() function. When it exits, it'll update the
-        // ax, ay, and az variables with the most current data.
-        imu.readAccel();
-    }
-    if ( imu.magAvailable() ) {
-        // To read from the magnetometer, first call the
-        // readMag() function. When it exits, it'll update the
-        // mx, my, and mz variables with the most current data.
-        imu.readMag();
-    }
-    tft.setTextColor(ST77XX_GREEN, ST77XX_BLACK);
-    tft.fillScreen(ST77XX_BLACK);
-    tft.setTextSize(1);
-    tft.setCursor(0, 0);
-    tft.print("ACC x:"); tft.print( imu.calcAccel(imu.ax)); tft.print(" y:"); tft.print(imu.calcAccel(imu.ay)); tft.print(" z:"); tft.println(imu.calcAccel(imu.az));
-    tft.print("GYR x:"); tft.print( imu.calcGyro(imu.gx)); tft.print(" y:"); tft.print(imu.calcGyro(imu.gy)); tft.print(" z:"); tft.println(imu.calcGyro(imu.gz));
-    tft.print("MAG x:"); tft.print( imu.calcMag(imu.mx)); tft.print(" y:"); tft.print(imu.calcMag(imu.my)); tft.print(" z:"); tft.println(imu.calcMag(imu.mz));
+
+
     delay(200);
 }
 
 
 void loop()
 {
+
+
     if (charge_indication) {
         charge_indication = false;
         if (digitalRead(CHARGE_PIN) == LOW) {
@@ -402,9 +337,10 @@ void loop()
             if (digitalRead(TFT_BL)) {
                 func_select = func_select + 1 > 2 ? 0 : func_select + 1;
             } else {
+                Serial.println("Trun on");
                 func_select = 0;
                 digitalWrite(TFT_BL, HIGH);
-                imu.sleepGyro(false);
+                // imu.sleepGyro(false);
                 tft.sendCommand(ST77XX_SLPOUT);
             }
             tft.fillScreen(ST77XX_BLACK);
@@ -426,9 +362,50 @@ void loop()
         break;
     case 2:
         if (digitalRead(TFT_BL)) {
-            imu.sleepGyro(true);
+            // imu.sleepGyro(true);
             tft.sendCommand(ST77XX_SLPIN);
             digitalWrite(TFT_BL, LOW);
+            setSleepEnabled(true);
+            SerialFlash.sleep();
+
+            sleep_enable = true;
+
+            sd_power_mode_set(NRF_POWER_MODE_LOWPWR);
+
+            attachInterrupt(TP_PIN_PIN, []() {
+                if (sleep_enable) {
+                    sleep_enable = false;
+                }
+            }, RISING);
+
+            NRF_UARTE0->ENABLE = 0; //disable UART
+            NRF_SAADC ->ENABLE = 0; //disable ADC
+            NRF_PWM0  ->ENABLE = 0; //disable all pwm instance
+            NRF_PWM1  ->ENABLE = 0;
+            NRF_PWM2  ->ENABLE = 0;
+            NRF_TWIM1 ->ENABLE = 0; //disable TWI Master
+            NRF_TWIS1 ->ENABLE = 0; //disable TWI Slave
+            NRF_SPI0 -> ENABLE = 0; //disable SPI
+            NRF_SPI1 -> ENABLE = 0; //disable SPI
+            NRF_SPI2 -> ENABLE = 0; //disable SPI
+
+            while (sleep_enable) {
+                __WFE();
+                __WFI();
+            }
+
+            detachInterrupt(TP_PIN_PIN);
+
+            NRF_UARTE0->ENABLE =    UARTE_ENABLE_ENABLE_Enabled;                               //disable UART
+            NRF_SPIM2->ENABLE =     (SPI_ENABLE_ENABLE_Enabled << SPI_ENABLE_ENABLE_Pos);;      //disable SPI
+            NRF_TWIM0->ENABLE  =    (TWIM_ENABLE_ENABLE_Enabled << TWIM_ENABLE_ENABLE_Pos);;  //disable TWI Master
+
+            tft.sendCommand(ST77XX_SLPOUT);
+            tft.sendCommand(ST77XX_DISPON);
+            digitalWrite(TFT_BL, HIGH);
+            setSleepEnabled(false);
+            SerialFlash.wakeup();
+            func_select = 0;
         }
         break;
     default:
@@ -436,4 +413,40 @@ void loop()
     }
 }
 
+#define MPU9250_ADDRESS     0x69
+#define PWR1_SLEEP_BIT      6
+#define MPU9250_PWR_MGMT_1  0x6B
 
+uint8_t readByte(uint8_t address, uint8_t subAddress)
+{
+    uint8_t data; // `data` will store the register data
+    Wire.beginTransmission(address);         // Initialize the Tx buffer
+    Wire.write(subAddress);                  // Put slave register address in Tx buffer
+    Wire.endTransmission(false);             // Send the Tx buffer, but send a restart to keep connection alive
+    Wire.requestFrom(address, (uint8_t) 1);  // Read one byte from slave register address
+    data = Wire.read();                      // Fill Rx buffer with result
+    return data;                             // Return data read from slave register
+}
+
+// Wire.h read and write protocols
+void writeByte(uint8_t address, uint8_t subAddress, uint8_t data)
+{
+    Wire.beginTransmission(address);  // Initialize the Tx buffer
+    Wire.write(subAddress);           // Put slave register address in Tx buffer
+    Wire.write(data);                 // Put data in Tx buffer
+    Wire.endTransmission();           // Send the Tx buffer
+}
+
+void writeBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t data)
+{
+    uint8_t b = readByte(devAddr, regAddr);
+    printf("rb:%x\n", b);
+    b = (data != 0) ? (b | (1 << bitNum)) : (b & ~(1 << bitNum));
+    printf("wb:%x\n", b);
+    writeByte(devAddr, regAddr, b);
+}
+
+void setSleepEnabled(bool enabled)
+{
+    writeBit(MPU9250_ADDRESS, MPU9250_PWR_MGMT_1, PWR1_SLEEP_BIT, enabled);
+}
